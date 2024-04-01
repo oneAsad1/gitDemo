7bcbb25d2ee9bef4ed8a3469c54d8c58ec1b1d6b
Asad Bashir <mabofficial33@gmail.com>
2024-04-01 09:50:01 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This line was added in test1 branch");
+        System.out.println("this line was added after merging and deleting test1 branch");
+    }
+}
